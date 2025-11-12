13221fe3069fe674deadfe46522113503fa04e13
Qoqondabitta <turgunalievabdulahad6554@gmail.com>
2025-11-12 00:12:33 -0500

    initial steps of website develompent

--- uzbekistan/src/root/Root.jsx
+++ uzbekistan/src/root/Root.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 
 const Root = () => {
   return (
-      <div>
-          
+    <div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
